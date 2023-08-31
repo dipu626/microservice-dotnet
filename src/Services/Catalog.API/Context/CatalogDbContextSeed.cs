f4f066741952f9cd5140b1dc580530650d8f972e
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-08-31 13:09:41 +0600

    Creating a Seed Method in .NET 7 with MongoDB for Catalog.API Microservice _ Setup Seed Data

---
+++ src/Services/Catalog.API/Context/CatalogDbContextSeed.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Manager;
+using Catalog.API.Models;
+
+namespace Catalog.API.Context
+{
+    public class CatalogDbContextSeed
+    {
+        static ProductManager _productManager = new ProductManager();
+
+        public static void Seed()
+        {
+            Product product = _productManager.GetFirstOrDefault(c => true);
+
+            if (product is null)
+            {
+                _productManager.Add(GetPreconfiguredProducts());
+            }
+        }
+
+        private static List<Product> GetPreconfiguredProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
