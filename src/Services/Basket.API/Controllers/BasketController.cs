8a4491d68faeea25808117c8226c4b03c67c402d
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-09-01 11:07:00 +0600

    Basket.API Microservice - Developing a High Scalable & Performance Basket Controller

---
+++ src/Services/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,68 @@
+﻿using Basket.API.Models;
+using Basket.API.Repositories;
+using CoreApiResponse;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BasketController : BaseController
+    {
+        private readonly IBasketRepository basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            this.basketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetBasket(string userName)
+        {
+            try
+            {
+                ShoppingCart basket = await basketRepository.GetBasketAsync(userName);
+
+                return CustomResult("Basket data load successfully.", basket);
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateBasket([FromBody]ShoppingCart basket)
+        {
+            try
+            {
+                ShoppingCart updatedBasket = await basketRepository.UpdateBasketAsync(basket);
+
+                return CustomResult("Basket modified done.", updatedBasket);
+            }
+            catch(Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            try
+            {
+                await basketRepository.DeleteBasketAsync(userName);
+
+                return CustomResult("Basket has been deleted.");
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
