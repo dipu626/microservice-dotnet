36a5ea11cecb5ac454cb0baa782b092ba48ccf59
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-09-01 10:09:31 +0600

    Basket.API Microservice Developing ShoppingCart Entity Model

---
+++ src/Services/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+        public decimal TotalPrice
+        {
+            get
+            {
+                return Items?.Sum(x => x.Price) ?? 0;
+            }
+        }
+    }
+}
