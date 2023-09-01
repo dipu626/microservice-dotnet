2e37c342a896a9306067cabd73334b0250b201bc
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-09-01 20:31:51 +0600

    DiscountService_ Implementing gRPC Proto Service with Discount Functionality

---
+++ src/Services/Discount.GRPC/Services/DiscountService.cs
@@ -0,0 +1,40 @@
+﻿using Discount.GRPC.Models;
+using Discount.GRPC.Protos;
+using Discount.GRPC.Repository;
+using Grpc.Core;
+
+namespace Discount.GRPC.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly ICouponRepository couponRepository;
+        private readonly ILogger<DiscountService> logger;
+
+        public DiscountService(ICouponRepository couponRepository, ILogger<DiscountService> logger)
+        {
+            this.couponRepository = couponRepository;
+            this.logger = logger;
+        }
+
+        public override async Task<CouponResponse> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            Coupon coupon = await this.couponRepository.GetDiscountAsync(request.ProductId);
+
+            if (coupon is null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, "Discount not found"));
+            }
+
+            this.logger.LogInformation($"Discount is retrived for ProductName: {coupon.ProductName}, Amount: {coupon.Amount}");
+
+            return new CouponResponse
+            {
+                Id = coupon.Id,
+                ProductId = coupon.ProductId,
+                ProductName = coupon.ProductName,
+                Description = coupon.Description,
+                Amount = coupon.Amount,
+            };
+        }
+    }
+}
