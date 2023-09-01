a1dd923ec2319ab7e5db7d1fa90ef6a14324ce62
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-09-01 20:43:43 +0600

    Efficient Object Mapping_ Implementing AutoMapper Web API _ Asp.Net Core with Mapper

---
+++ src/Services/Discount.GRPC/MappingProfies/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Discount.GRPC.Models;
+using Discount.GRPC.Protos;
+
+namespace Discount.GRPC.MappingProfies
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Coupon, CouponRequest>().ReverseMap();
+            CreateMap<Coupon, CouponResponse>().ReverseMap();
+        }
+    }
+}
--- src/Services/Discount.GRPC/Services/DiscountService.cs
+++ src/Services/Discount.GRPC/Services/DiscountService.cs
@@ -1,4 +1,5 @@
-﻿using Discount.GRPC.Models;
+﻿using AutoMapper;
+using Discount.GRPC.Models;
 using Discount.GRPC.Protos;
 using Discount.GRPC.Repository;
 using Grpc.Core;
@@ -9,11 +10,13 @@ namespace Discount.GRPC.Services
     {
         private readonly ICouponRepository couponRepository;
         private readonly ILogger<DiscountService> logger;
+        private readonly IMapper mapper;
 
-        public DiscountService(ICouponRepository couponRepository, ILogger<DiscountService> logger)
+        public DiscountService(ICouponRepository couponRepository, ILogger<DiscountService> logger, IMapper mapper)
         {
             this.couponRepository = couponRepository;
             this.logger = logger;
+            this.mapper = mapper;
         }
 
         public override async Task<CouponResponse> GetDiscount(GetDiscountRequest request, ServerCallContext context)
@@ -27,14 +30,16 @@ namespace Discount.GRPC.Services
 
             this.logger.LogInformation($"Discount is retrived for ProductName: {coupon.ProductName}, Amount: {coupon.Amount}");
 
-            return new CouponResponse
-            {
-                Id = coupon.Id,
-                ProductId = coupon.ProductId,
-                ProductName = coupon.ProductName,
-                Description = coupon.Description,
-                Amount = coupon.Amount,
-            };
+            //return new CouponResponse
+            //{
+            //    Id = coupon.Id,
+            //    ProductId = coupon.ProductId,
+            //    ProductName = coupon.ProductName,
+            //    Description = coupon.Description,
+            //    Amount = coupon.Amount,
+            //};
+
+            return this.mapper.Map<CouponResponse>(coupon);
         }
     }
 }
