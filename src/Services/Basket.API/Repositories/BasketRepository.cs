dbb7db2a48fbb4d4b12b66c6c5dffb7da1e09a26
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-09-01 10:30:06 +0600

    Basket.API Microservice - Implementing a Repository Pattern with Redis

---
+++ src/Services/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,42 @@
+﻿using Basket.API.Models;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            this.redisCache = redisCache;
+        }
+
+        public async Task<ShoppingCart> GetBasketAsync(string userName)
+        {
+            string basket = await redisCache.GetStringAsync(userName);
+
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasketAsync(ShoppingCart basket)
+        {
+            string value = JsonConvert.SerializeObject(basket);
+
+            await redisCache.SetStringAsync(basket.UserName, value);
+
+            return await GetBasketAsync(basket.UserName);
+        }
+
+        public async Task DeleteBasketAsync(string userName)
+        {
+            await redisCache.RemoveAsync(userName);
+        }
+    }
+}
